21bbf0544ec384ae339b2f1850d8fd1725478652
HSP48375 <jason@jnaudiphotography.com>
2025-06-19 18:56:23 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './screens/WelcomeScreen';
-
-const Stack = createNativeStackNavigator();
+import UploadScreen from './screens/UploadScreen';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Welcome'>
-        <Stack.Screen name='Welcome' component={WelcomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <UploadScreen />;
 }
-
